085220520700a6d2ef8386eaff7785df1efed3f7
Dixon <dixon.lee527@gmail.com>
2023-04-05 00:49:12 +0800

    Adeed playRound function that let user select rock,paper,scissors and a if statement that define who win.

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function getComputerChoice() {
-    const item = ["Rock", "Paper", "Scissors"]
-    const computerChoice = item [Math.floor(Math.random() * item.length)];
+    const item = ["rock", "paper", "scissors"]
+    const computerChoice = item[Math.floor(Math.random() * item.length)];
     return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("Tie");
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return ("You Win! Rock beats scissors!");
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return ("You Win! Paper beats rock!");
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return ("You Win! Scissors beats paper!");
+    } else if (computerSelection == "rock" && playerSelection == "scissors") {
+        return ("You Lose! Rocks beats scirsors!");
+    } else if (computerSelection == "paper" && playerSelection == "rock") {
+        return ("You Lose! Paper beats rock!");
+    } else if (computerSelection == "scissors" && playerSelection == "paper") {
+        return ("You Lose! Scissors beats paper!");
+    } else { return ("error") }
+}
+
+let playerSelection = prompt("Rock, Paper, Scissors?")
+const computerSelection = getComputerChoice();
+console.log("You pick " + playerSelection)
+console.log("computer picks " + computerSelection)
+console.log(playRound(playerSelection, computerSelection))
 
