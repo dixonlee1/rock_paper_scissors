344de9038724b2f9e26559bc284d8596688a3560
Dixon <dixon.lee527@gmail.com>
2023-04-04 23:06:39 +0800

    Created getComputerChoice that will randomly return rock paper scissors

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function getComputerChoice() {
-    
+    const item = ["Rock", "Paper", "Scissors"]
+    const computerChoice = item [Math.floor(Math.random() * item.length)];
+    return computerChoice;
 }
+
